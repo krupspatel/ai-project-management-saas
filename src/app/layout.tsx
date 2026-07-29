0833e959b3dae1808453e6e744fb8883631e2fc2
Krupali <krupali.patel@tradatomy.com>
2026-07-29 15:24:13 +0200

    chore: setup scalable src architecture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Inter } from 'next/font/google';
-    
+
 import './globals.css';
 import { cn } from '@/lib/utils';
 
@@ -14,7 +14,7 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-}); 
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
